e0ab2a140a07c17fb9b295d68caa466997c4e8fe
ghktndyd <hwasuyong99@naver.com>
2024-02-13 14:13:35 +0900

    removed cached

---
+++ src/programmers/문자열을정수로.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class 문자열을정수로 {
+    class Solution {
+        public int solution(String n_str) {
+            int answer = 0;
+
+            answer = Integer.parseInt(n_str);
+
+            return answer;
+        }
+    }
+}
