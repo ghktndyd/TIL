a58ffe8e1ac9b179b6871b327046f94555607121
ghktndyd <hwasuyong99@naver.com>
2024-02-14 16:32:12 +0900

    자바의 정석 복습

---
+++ src/javaexam/ArrayEx5.java
@@ -0,0 +1,19 @@
+package javaexam;
+
+public class ArrayEx5 {
+    public static void main(String[] args) {
+        int sum = 0;
+        float avg;
+
+        int[] scores = {100, 88, 90, 87, 90};
+
+        for (int score : scores) {
+            sum += score;
+        }
+
+        avg = (float) sum / scores.length;
+
+        System.out.println("sum = " + sum);
+        System.out.println("avg = " + avg);
+    }
+}
