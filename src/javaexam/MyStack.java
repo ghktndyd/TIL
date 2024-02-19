7ee6a32a0c84b795f4b31c965863c78120580a4a
ghktndyd <hwasuyong99@naver.com>
2024-02-19 15:37:21 +0900

    스택과 큐

---
+++ src/javaexam/MyStack.java
@@ -0,0 +1,42 @@
+package javaexam;
+
+import java.util.EmptyStackException;
+import java.util.Vector;
+
+public class MyStack extends Vector {
+
+    public Object push(Object item) {
+        addElement(item);
+        return item;
+    }
+
+    public Object pop() {
+        Object obj = peek();
+        removeElement(size() - 1);
+        return obj;
+    }
+
+    private Object peek() {
+        int len = size();
+
+        if (len == 0) {
+            throw new EmptyStackException();
+        }
+
+        return elementAt(len - 1);
+    }
+
+    public boolean empty() {
+        return size() == 0;
+    }
+
+    public int search(Object o) {
+        int i = lastIndexOf(o); // 반환값은 저장된 위치
+
+        if (i >= 0) {
+            return size() - 1;
+        }
+
+        return -1; // 못 찾았을시
+    }
+}
