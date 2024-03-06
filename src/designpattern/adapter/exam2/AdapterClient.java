7198c79e0d991e25956ec5a00a942ddabe0610e3
ghktndyd <hwasuyong99@naver.com>
2024-03-06 18:57:46 +0900

    알고리즘

--- src/designpattern/adapter/exam2/AdapterClient.java
+++ src/designpattern/adapter/exam2/AdapterClient.java
@@ -3,9 +3,9 @@ package designpattern.adapter.exam2;
 public class AdapterClient {
     public static void main(String[] args) {
         LegacyXmlService legacyXmlService = new LegacyXmlService();
+
         JsonService jsonService = new XmlToJsonAdapter(legacyXmlService);
 
         System.out.println(jsonService.getJson());
-
     }
 }
