c583c7b995a0917778b0b0f5c00d23247277fc74
ghktndyd <hwasuyong99@naver.com>
2024-05-08 15:30:52 +0900

    ArrayList

---
+++ java/src/javaexam/collection/ListSample.java
@@ -0,0 +1,17 @@
+package javaexam.collection;
+
+import java.util.ArrayList;
+
+public class ListSample {
+    public static void main(String[] args) {
+        ListSample listSample = new ListSample();
+        listSample.checkArrayList1();
+    }
+
+    private void checkArrayList1() {
+        ArrayList<Object> list1 = new ArrayList<>();
+        list1.add(new Object());
+        list1.add("ArrayListSample");
+    }
+
+}
