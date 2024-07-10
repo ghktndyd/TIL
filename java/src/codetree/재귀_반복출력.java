ad122b1be2424f254dc75067505bc03a6f4e5f3c
ghktndyd <hwasuyong99@naver.com>
2024-07-10 18:37:41 +0900

    재귀 연습

---
+++ java/src/codetree/재귀_반복출력.java
@@ -0,0 +1,20 @@
+package codetree;
+
+import java.util.Scanner;
+
+public class 재귀_반복출력 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        print(sc.nextInt());
+    }
+
+    static void print(int N) {
+        if (N == 0) {
+            return;
+        }
+
+        print(N - 1);
+        System.out.println("HelloWorld");
+    }
+}
---
+++ java/src/codetree/재귀_별찍기.java
@@ -0,0 +1,24 @@
+package codetree;
+
+import java.util.Scanner;
+
+public class 재귀_별찍기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        printStar(sc.nextInt());
+    }
+
+    private static void printStar(int count) {
+        if (count == 0) {
+            return;
+        }
+
+        printStar(count - 1);
+        for (int i = 0; i < count; i++) {
+            System.out.print("*");
+        }
+
+        System.out.println();
+    }
+}
