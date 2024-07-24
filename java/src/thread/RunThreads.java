f3f6cc28a3bb0b0ee4e5378a73b3dda1b40380dd
ghktndyd <hwasuyong99@naver.com>
2024-07-24 20:03:04 +0900

    쓰레드 샘플

---
+++ java/src/thread/RunThreads.java
@@ -0,0 +1,17 @@
+package thread;
+
+public class RunThreads {
+    public static void main(String[] args) {
+        RunThreads threads = new RunThreads();
+        threads.runBasic();
+    }
+
+    private void runBasic() {
+        RunnableSample runnableSample = new RunnableSample();
+        new Thread(runnableSample).start();
+
+        ThreadSample threadSample = new ThreadSample();
+        threadSample.start();
+        System.out.println("RunThreads.runBasic() method is ended");
+    }
+}
