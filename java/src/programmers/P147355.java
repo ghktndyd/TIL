0b268df74afff0b1fbcec7bf3dfa994cdcd93b90
ghktndyd <hwasuyong99@naver.com>
2024-12-28 13:53:14 +0900

    프로그래머스 - 크기가 작은 부분 문자열

--- java/src/programmers/P147355.java
+++ java/src/programmers/P147355.java
@@ -1,4 +1,24 @@
 package programmers;
 
 public class P147355 {
+    public int solution(String t, String p) {
+        int length = p.length();
+        long number = Long.parseLong(p);
+
+        int count = 0;
+        for (int i = 0; i < t.length() - length + 1; i++) {
+            String substring = t.substring(i, i + length);
+            if (Long.parseLong(substring) <= number) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+        return count;
+    }
+
+    public static void main(String[] args) {
+        P147355 p147355 = new P147355();
+        p147355.solution("10203", "15");
+    }
 }
