a01b68c8dbee6aaa0430b02f662f072154bc81e0
ghktndyd <hwasuyong99@naver.com>
2025-01-01 15:31:03 +0900

    프로그래머스 - 카드뭉치

--- java/src/programmers/P159994.java
+++ java/src/programmers/P159994.java
@@ -1,4 +1,24 @@
 package programmers;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class P159994 {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        Queue<String> card1 = new LinkedList<>(Arrays.asList(cards1));
+        Queue<String> card2 = new LinkedList<>(Arrays.asList(cards2));
+
+        for (String word : goal) {
+            if (!card1.isEmpty() && card1.peek().equals(word)) {
+                card1.poll();
+            } else if (!card2.isEmpty() && card2.peek().equals(word)) {
+                card2.poll();
+            } else {
+                return "No";
+            }
+        }
+
+        return "Yes";
+    }
 }
